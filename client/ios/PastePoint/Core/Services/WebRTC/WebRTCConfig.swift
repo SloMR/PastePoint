c1153517478e009547375aa7eb67fa6587dd93e9
Sulaiman AlRomaih <xSloMxSA@gmail.com>
2026-05-15 21:08:22 +0300

    iOS: Expands the list of ICE servers to include multiple Google STUN, public STUN, and Open Relay Project TURN servers.

--- client/ios/PastePoint/Core/Services/WebRTC/WebRTCConfig.swift
+++ client/ios/PastePoint/Core/Services/WebRTC/WebRTCConfig.swift
@@ -11,7 +11,28 @@ enum WebRTCConfig {
   static let bufferedAmountLowThreshold: UInt64 = 1 * 1024 * 1024 // 1MB
   
   static let iceServers: [RTCIceServer] = [
-    RTCIceServer(urlStrings: ["stun:stun.l.google.com:19302"]) // TODO: Add more RTC Servers.
+    RTCIceServer(urlStrings: [
+      // Google STUN servers
+      "stun:stun.l.google.com:19302",
+      "stun:stun1.l.google.com:19302",
+      "stun:stun2.l.google.com:19302",
+      "stun:stun3.l.google.com:19302",
+      "stun:stun4.l.google.com:19302",
+      // Third-party/public STUN servers
+      "stun:stun.voipbuster.com",
+      "stun:stun.services.mozilla.com",
+      "stun:stun.stunprotocol.org:3478",
+      "stun:stun.iptel.org",
+    ]),
+
+    // Open Relay Project TURN servers
+    RTCIceServer(urlStrings: [
+      "turn:openrelay.metered.ca:80",
+      "turn:openrelay.metered.ca:443",
+      "turns:openrelay.metered.ca:443",],
+                 username: "openrelayproject",
+                 credential: "openrelayproject")
+
   ]
   
   static let peerConnectionConfig: RTCConfiguration = {
