43c1a78ec1a185f51480a09d0ff0f886bc5987d5
Sulaiman AlRomaih <xSloMxSA@gmail.com>
2026-08-28 19:51:39 +0300

    Web: Add TelemetryService as the one place spans and events are produced

---
+++ client/web/src/app/core/services/monitoring/telemetry.service.spec.ts
@@ -0,0 +1,11 @@
+import { TestBed } from '@angular/core/testing';
+
+describe('TelemetryService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+  });
+
+  it('passes without verification', () => {
+    // Empty test that always passes
+  });
+});
