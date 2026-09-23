611e8edb5346806c48832ed09dbe930ea5a3c459
Sulaiman AlRomaih <xSloMxSA@gmail.com>
2026-09-24 00:22:46 +0300

    Nginx: Keep the entrypoint, template and static files read-only to nginx
    - nginx owned them, so anything running as nginx could rewrite the entrypoint or template and survive a restart

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Substitute environment variables in the nginx config template
-envsubst "\$SERVER_NAME \$SSL_CERT_PATH \$SSL_CERT_KEY_PATH" </etc/nginx/conf.d/default.conf.template >/etc/nginx/conf.d/default.conf
+envsubst "\$SERVER_NAME \$SSL_CERT_PATH \$SSL_CERT_KEY_PATH" </etc/nginx/templates/default.conf.template >/etc/nginx/conf.d/default.conf
 echo "Starting Nginx..."
 
 # Execute the main command
